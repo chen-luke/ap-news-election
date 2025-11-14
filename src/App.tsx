1f48e9d9b843e567a56c97e378894bc9407e10ad
chen-luke <72319988+chen-luke@users.noreply.github.com>
2025-11-14 13:31:37 -0500

    created initial tool bar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Nav from './components/nav/Nav';
+import ToolBar from './components/toolbar/ToolBar';
 
 function App() {
   return (
     <>
       <div>
         <Nav />
+        <ToolBar />
         <h1>2025 General Election Results</h1>
         <h3>Key races</h3>
         <div>
---
+++ src/components/toolbar/ToolBar.tsx
@@ -0,0 +1,53 @@
+import styled from 'styled-components';
+import StateDropdown from './StatesDropdown';
+
+const StyledLogo = styled.img`
+  width: 100%;
+`;
+
+const StyledLogoContainer = styled.div`
+  display: flex;
+  width: 110px;
+  align-items: center;
+  margin-left: 15px;
+  cursor: pointer;
+`;
+
+const StyledToolBar = styled.div`
+  display: grid;
+  height: 3rem;
+  background-color: #000;
+  padding: 0.5rem 0;
+  border-top: 1px solid #696969;
+  grid-template-columns: 1fr 3fr 1fr;
+`;
+
+const StyledHeaderLinks = styled.div`
+  display: flex;
+  justify-content: center;
+  gap: 2rem;
+  align-items: center;
+  cursor: pointer;
+`;
+
+const StyledLinks = styled.a`
+  color: white;
+  font-weight: 300;
+`;
+
+const ToolBar = () => {
+  return (
+    <StyledToolBar>
+      <StyledLogoContainer>
+        <StyledLogo src='./public/AP_Elections_B2C_2025_logo_small_dbg_RGB.svg'></StyledLogo>
+      </StyledLogoContainer>
+      <StyledHeaderLinks>
+        <StyledLinks>EARLY VOTING</StyledLinks>
+        <StyledLinks>LIVE ELECTION UPDATES</StyledLinks>
+      </StyledHeaderLinks>
+      <StateDropdown />
+    </StyledToolBar>
+  );
+};
+
+export default ToolBar;
