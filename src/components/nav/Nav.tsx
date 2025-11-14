ca671345aa2d136116d2d6fd6e58657640511482
chen-luke <72319988+chen-luke@users.noreply.github.com>
2025-11-13 22:46:03 -0500

    created style for nav bar

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -1,17 +1,56 @@
-import styles from './nav.module.css';
+import styled from 'styled-components';
+
+const StyledWrapper = styled.div`
+  background-color: #000;
+`;
+const StyledContainer = styled.div`
+  display: grid;
+  grid-template-columns: 106px 1fr 106px;
+  align-items: center;
+  max-width: 1440px;
+  margin: 0 auto;
+  position: relative;
+  height: 55px;
+`;
+
+const StyledUl = styled.ul`
+  margin: 0;
+  padding: 0;
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+  list-style: none;
+`;
+
+const Logo = styled.img`
+  width: 48px;
+`;
+
+const DonateButton = styled.a`
+  line-height: 0.625rem;
+  font-style: normal;
+  font-size: 0.875rem;
+  font-weight: 550;
+  background-color: #eb483b;
+  text-transform: uppercase;
+  color: #fff;
+  border-radius: 4px;
+  margin-right: 20px;
+  padding: 8px 12px;
+  cursor: pointer;
+  text-decoration: none;
+`;
 
 const Nav = () => {
   return (
     <nav>
-      <div className={styles.wrapper}>
-        <div className={styles.container}>
+      <StyledWrapper>
+        <StyledContainer>
           <a href=''>
-            <picture>
-              <source />
-              <img src='public\ap-logo-176-by-208.svg' alt='' />
-            </picture>
+            <Logo src='public\ap-logo-176-by-208.svg' alt='' />
           </a>
-          <ul style={{ display: 'flex' }}>
+          <StyledUl>
             <li>World</li>
             <li>U.S.</li>
             <li>Politics</li>
@@ -20,10 +59,10 @@ const Nav = () => {
             <li>Business</li>
             <li>Fact Check</li>
             <li>Newsletters</li>
-          </ul>
-          <a>Donate</a>
-        </div>
-      </div>
+          </StyledUl>
+          <DonateButton>Donate</DonateButton>
+        </StyledContainer>
+      </StyledWrapper>
     </nav>
   );
 };
