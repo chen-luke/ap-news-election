816035135a67aa5f829b8bba500ec7079384155b
chen-luke <72319988+chen-luke@users.noreply.github.com>
2025-11-19 16:09:59 -0500

    created other races component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
 import KeyRaceCards from './components/keyrace/KeyRaceCards';
 import Nav from './components/nav/Nav';
+import OtherRaces from './components/other-race/OtherRaces';
 import ToolBar from './components/toolbar/ToolBar';
+import styled from 'styled-components';
+
+const MainContainer = styled.main`
+  display: grid;
+  margin: 0 auto;
+  gap: 0px;
+  max-width: 1030px;
+`;
 
 function App() {
   return (
@@ -8,11 +17,12 @@ function App() {
       <div>
         <Nav />
         <ToolBar />
-        <main>
+        <MainContainer>
           <h1>2025 General Election Results</h1>
-          <h3>Key races</h3>
+          <h2>Key races</h2>
           <KeyRaceCards />
-        </main>
+          <OtherRaces />
+        </MainContainer>
       </div>
     </>
   );
---
+++ src/components/other-race/OtherRaces.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+import RaceLegend from './RaceLegend';
+
+const StyledWrapper = styled.div``;
+
+export default function OtherRaces() {
+  return (
+    <StyledWrapper>
+      <h2>More Nov.4 races</h2>
+      <RaceLegend />
+    </StyledWrapper>
+  );
+}
