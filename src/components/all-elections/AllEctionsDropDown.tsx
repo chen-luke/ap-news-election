294b2de4f63f3b6cf65628018a82d08c1c294b66
chen-luke <72319988+chen-luke@users.noreply.github.com>
2025-11-24 17:25:25 -0500

    Refactor AllElectionsDropDown to use RaceDropdown component and improve race icon rendering

--- src/components/all-elections/AllEctionsDropDown.tsx
+++ src/components/all-elections/AllEctionsDropDown.tsx
@@ -2,13 +2,11 @@ import styled from 'styled-components';
 
 import type {
   ElectionDayFormatted,
-  RaceDetail,
   StateElectionGroups,
 } from '../../voting-data';
 import { usElectionSchedule2025 } from '../../voting-data';
 import { v4 as uuidv4 } from 'uuid';
-import RaceTypeIcons from './RaceTypeIcons';
-import { useState } from 'react';
+import RaceDropdown from './RaceDropdown';
 
 const DropdownContainer = styled.div`
   display: grid;
@@ -16,95 +14,28 @@ const DropdownContainer = styled.div`
 `;
 const Dropdown = styled.div``;
 
-const ExpandButton = styled.button`
-  font-size: 25px;
-  font-weight: 700;
-  line-height: 0.9;
-  background-color: transparent;
-  border-radius: 6px;
-  transition: background-color 0.15s;
-  cursor: pointer;
-  height: 28px;
-  width: 28px;
-  display: flex;
-  place-content: center;
-  color: black;
-  border: 0;
-  margin: 0;
-  padding: 0;
-
-  &:hover {
-    background-color: #c5c5c5ff;
-  }
-`;
-
 export default function AllElectionsDropDown() {
-  const [isExpanded, setIsExpanded] = useState(false);
-
-  const toggleDropdown = () => setIsExpanded(!isExpanded);
-
   return (
     <DropdownContainer>
       <Dropdown>
         {usElectionSchedule2025.map((day: ElectionDayFormatted) => (
-          <div key={uuidv4()}>
-            {day.date}
-            {day.races.map((race: StateElectionGroups) => (
-              <div key={uuidv4()}>
-                <div
-                  style={{
-                    display: 'flex',
-                    alignItems: 'center',
-                    gap: '8px',
-                    justifyContent: 'space-between',
-                  }}
-                >
-                  <h3
-                    style={{
-                      display: 'flex',
-                      alignItems: 'center',
-                      gap: '8px',
-                      margin: '2px 0px 4px',
-                    }}
-                  >
-                    {race.state}
-                    <RaceTypeIcons race={race} />
-                  </h3>
-                  {isExpanded ? (
-                    <ExpandButton onClick={toggleDropdown}>-</ExpandButton>
-                  ) : (
-                    <ExpandButton onClick={toggleDropdown}>+</ExpandButton>
-                  )}
-                </div>
-                <hr />
-                <div>
-                  {Object.entries(race).map(([key, value]) => {
-                    if (key === 'state' || !value) return null;
-
-                    const raceDetails = value as RaceDetail[];
-
-                    return (
-                      <div key={uuidv4()}>
-                        {isExpanded &&
-                          raceDetails.map((detail: RaceDetail) => {
-                            return (
-                              <div key={uuidv4()}>
-                                <a
-                                  href=''
-                                  style={{ textDecoration: 'underline' }}
-                                >
-                                  <b>{detail.district}</b>
-                                </a>{' '}
-                                {detail.description}
-                              </div>
-                            );
-                          })}
-                      </div>
-                    );
-                  })}
-                </div>
-              </div>
-            ))}
+          <div key={uuidv4()} style={{ marginTop: '10px' }}>
+            <span style={{ fontWeight: '300' }}>{day.date}</span>
+            <hr style={{ marginBottom: '5px', marginTop: '5px' }} />
+            {day.races.map(
+              (
+                race: StateElectionGroups,
+                index: number,
+                array: StateElectionGroups[]
+              ) => (
+                <RaceDropdown
+                  key={uuidv4()}
+                  race={race}
+                  index={index}
+                  length={array.length}
+                />
+              )
+            )}
           </div>
         ))}
       </Dropdown>
--- src/components/all-elections/RaceTypeIcons.tsx
+++ src/components/all-elections/RaceTypeIcons.tsx
@@ -2,40 +2,47 @@ import type { JSX } from 'react';
 import type { StateElectionGroups } from '../../voting-data';
 import { v4 as uuidv4 } from 'uuid';
 
-const usHouseElectionIcon = (
-  <svg
-    width='12'
-    height='12'
-    viewBox='0 0 12 12'
-    fill='none'
-    xmlns='http://www.w3.org/2000/svg'
-  >
-    <rect
-      width='7.95592'
-      height='7.95592'
-      transform='matrix(0.703168 -0.711024 0.703168 0.711024 0.328293 6)'
-      fill='#494949'
-    ></rect>
-  </svg>
-);
+const HouseElectionIcon = (props: JSX.IntrinsicElements['svg']) => {
+  return (
+    <svg
+      {...props}
+      width='12'
+      height='12'
+      viewBox='0 0 12 12'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <rect
+        width='7.95592'
+        height='7.95592'
+        transform='matrix(0.703168 -0.711024 0.703168 0.711024 0.328293 6)'
+        fill='#494949'
+      ></rect>
+    </svg>
+  );
+};
 
-const usGovernorElectionIcon = (
-  <svg
-    width='15'
-    height='14'
-    viewBox='0 0 15 14'
-    fill='none'
-    xmlns='http://www.w3.org/2000/svg'
-  >
-    <path
-      d='M7.5 0L9.18386 5.18237H14.6329L10.2245 8.38525L11.9084 13.5676L7.5 10.3647L3.09161 13.5676L4.77547 8.38525L0.367076 5.18237H5.81614L7.5 0Z'
-      fill='#494949'
-    ></path>
-  </svg>
-);
+const GovernorElectionIcon = (props: JSX.IntrinsicElements['svg']) => {
+  return (
+    <svg
+      {...props}
+      width='15'
+      height='14'
+      viewBox='0 0 15 14'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <path
+        d='M7.5 0L9.18386 5.18237H14.6329L10.2245 8.38525L11.9084 13.5676L7.5 10.3647L3.09161 13.5676L4.77547 8.38525L0.367076 5.18237H5.81614L7.5 0Z'
+        fill='#494949'
+      ></path>
+    </svg>
+  );
+};
 
-const usMayorElectionIcon = (
+const MayorElectionIcon = (props: JSX.IntrinsicElements['svg']) => (
   <svg
+    {...props}
     width='8'
     height='8'
     viewBox='0 0 8 8'
@@ -46,23 +53,26 @@ const usMayorElectionIcon = (
   </svg>
 );
 
-const usBallotMeasureIcon = (
-  <svg
-    width='8'
-    height='8'
-    viewBox='0 0 10 10'
-    fill='none'
-    xmlns='http://www.w3.org/2000/svg'
-  >
-    <rect width='10' height='10' fill='#494949'></rect>
-  </svg>
-);
+const BallotMeasureIcon = (props: JSX.IntrinsicElements['svg']) => {
+  return (
+    <svg
+      {...props}
+      width='8'
+      height='8'
+      viewBox='0 0 10 10'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <rect width='10' height='10' fill='#494949'></rect>
+    </svg>
+  );
+};
 
 const getRaceIcon = (raceType: string) => {
-  if (raceType === 'houseRace') return usHouseElectionIcon;
-  if (raceType === 'govRace') return usGovernorElectionIcon;
-  if (raceType === 'mayorRace') return usMayorElectionIcon;
-  if (raceType === 'ballotMeasure') return usBallotMeasureIcon;
+  if (raceType === 'houseRace') return HouseElectionIcon;
+  if (raceType === 'govRace') return GovernorElectionIcon;
+  if (raceType === 'mayorRace') return MayorElectionIcon;
+  if (raceType === 'ballotMeasure') return BallotMeasureIcon;
   return null;
 };
 
@@ -77,11 +87,13 @@ export default function RaceTypeIcons({
     <div style={{ display: 'flex', gap: '4px', alignItems: 'center' }}>
       {Object.entries(race).map(([key, value]) => {
         if (key === 'state' || !value) return null;
+
+        const IconComponent = getRaceIcon(key);
+
+        if (!IconComponent || !Array.isArray(value)) return null;
         return (
           Array.isArray(value) &&
-          value.map(() => {
-            return <div key={uuidv4()}>{getRaceIcon(key)}</div>;
-          })
+          value.map(() => <IconComponent key={uuidv4()} />)
         );
       })}
     </div>
