2f327e36ffb32b32211cbd3792b73b5c377ca031
chen-luke <72319988+chen-luke@users.noreply.github.com>
2025-11-24 21:15:51 -0500

    Refactor FilterBox component to utilize dynamic categories and improve icon rendering

--- src/components/all-elections/FilterBox.tsx
+++ src/components/all-elections/FilterBox.tsx
@@ -1,8 +1,16 @@
 import styled from 'styled-components';
+import getRaceIcon from './AllElection.util';
 
 const CategoryContainer = styled.div`
   display: grid;
 `;
+
+const categories = [
+  { name: 'U.S. HOUSE', icon: 'houseRace' },
+  { name: 'GOVERNOR', icon: 'govRace' },
+  { name: 'MAYOR', icon: 'mayorRace' },
+  { name: 'BALLOT MEASURE', icon: 'ballotRace' },
+];
 
 const Category = styled.button`
   display: flex;
@@ -36,7 +44,11 @@ const FilterContainer = styled.div`
   background-color: rgb(255, 255, 255);
 `;
 
-export default function FilterBox() {
+interface FilterBoxProps {
+  onFilterChange: (key: string | null) => void;
+}
+
+export default function FilterBox({ onFilterChange }: FilterBoxProps) {
   return (
     <FilterContainer>
       <div>
@@ -44,62 +56,16 @@ export default function FilterBox() {
       </div>
       <hr />
       <CategoryContainer>
-        <Category>
-          <svg
-            width='12'
-            height='12'
-            viewBox='0 0 12 12'
-            fill='none'
-            xmlns='http://www.w3.org/2000/svg'
-          >
-            <rect
-              width='7.95592'
-              height='7.95592'
-              transform='matrix(0.703168 -0.711024 0.703168 0.711024 0.328293 6)'
-              fill='#494949'
-            ></rect>
-          </svg>
-          <FilterBoxText>U.S. HOUSE</FilterBoxText>
-        </Category>
-        <Category>
-          <svg
-            width='15'
-            height='14'
-            viewBox='0 0 15 14'
-            fill='none'
-            xmlns='http://www.w3.org/2000/svg'
-          >
-            <path
-              d='M7.5 0L9.18386 5.18237H14.6329L10.2245 8.38525L11.9084 13.5676L7.5 10.3647L3.09161 13.5676L4.77547 8.38525L0.367076 5.18237H5.81614L7.5 0Z'
-              fill='#494949'
-            ></path>
-          </svg>
-          <FilterBoxText>GOVERNOR</FilterBoxText>
-        </Category>
-        <Category>
-          <svg
-            width='8'
-            height='8'
-            viewBox='0 0 8 8'
-            fill='none'
-            xmlns='http://www.w3.org/2000/svg'
-          >
-            <circle cx='4' cy='4' r='4' fill='#494949'></circle>
-          </svg>
-          <FilterBoxText>MAYOR</FilterBoxText>
-        </Category>
-        <Category>
-          <svg
-            width='8'
-            height='8'
-            viewBox='0 0 10 10'
-            fill='none'
-            xmlns='http://www.w3.org/2000/svg'
-          >
-            <rect width='10' height='10' fill='#494949'></rect>
-          </svg>
-          <FilterBoxText>BALLOT MEASURE</FilterBoxText>
-        </Category>
+        {categories.map((category, index) => {
+          const IconComponent = getRaceIcon(category.icon);
+          return (
+            <Category key={index} onClick={() => onFilterChange(category.name)}>
+              {IconComponent && <IconComponent />}
+              {/* Only render if IconComponent is not null */}
+              <FilterBoxText>{category.name}</FilterBoxText>
+            </Category>
+          );
+        })}
       </CategoryContainer>
     </FilterContainer>
   );
